0744aaa3574899508cb450030ec8f656dd20a3d0
Tanmay Pereira Naik <tanmay36rocks@gmail.com>
2017-12-04 00:49:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+for (var x=1; x <= 100; x++){
+	console.log(x);
+    	if( x % 3 == 0 ) {
+        	console.log('Earth');	
+    	}
+	if( x % 5 == 0 ) {
+        	console.log('Jupiter');	
+    	}
+	if( x % 9 == 0 ) {
+        	console.log('Pluto');	
+    	}
+}​
